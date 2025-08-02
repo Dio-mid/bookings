4c10b850e1895bbef8b5308fa62a9928ed783098
Dio-mid <dbuharevich@gmail.com>
2025-08-02 20:01:16 +0300

    second commit

---
+++ hotels.py
@@ -0,0 +1,95 @@
+from fastapi import Query, APIRouter, Body
+from schemes.hotels import Hotel, HotelPatch
+
+router = APIRouter(prefix="/hotels", tags=["Отели"])
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
+]
+
+
+@router.get("")
+def get_hotels(
+        id: int | None = Query(None, description="Айдишник"),
+        title: str | None = Query(None, description="Название отеля"),
+        page: int | None = Query(1, description="Номер страницы"),
+        per_page: int | None = Query(3, description="Количество элементов на странице")
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+
+    start_index = (page - 1) * per_page
+    end_index = start_index + per_page
+    paginated_hotels = hotels_[start_index:end_index]
+    return paginated_hotels
+
+
+@router.post("")
+def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+    "1":{
+        "summary": "Сочи",
+        "value": {
+            "title": "Отель Сочи 5 звезд у моря",
+            "name": "sochi_u_morya",
+        }
+    },
+    "2": {
+        "summary": "Дубай",
+        "value": {
+            "title": "Отель Дубай У фонтана",
+            "name": "dubai_fountain",
+        }
+    }
+})
+): #Body(embed=True) для передачи именно JSON и для только одного параметра
+    global hotels
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": hotel_data.title,
+        "name": hotel_data.name,
+    })
+    return {"status": "OK"} # нужно возвращать JSON формат, а не строку
+
+
+@router.delete("/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"} # нужно возвращать JSON формат, а не строку
+
+
+@router.put("/{hotel_id}")
+def put_hotels(hotel_id: int, hotel_data: Hotel):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            hotel["title"] = hotel_data.title
+            hotel["name"] = hotel_data.name
+
+    return {"status": "OK"}
+
+
+@router.patch("/{hotel_id}",
+           summary="Частичное обновление данных",
+           description="<h1>Тут мы частично обновляем данные об отеле: можно title и/или name</h1>")
+def patch_hotel(hotel_id: int,
+               hotel_data: HotelPatch):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == hotel_id and hotel_data.title:
+            hotel["title"] = hotel_data.title
+        if hotel["id"] == hotel_id and hotel_data.name:
+            hotel["name"] = hotel_data.name
+
+    return {"status": "OK"}
--- main.py
+++ main.py
@@ -1,75 +1,12 @@
 from fastapi import FastAPI, Query, Body
 import uvicorn
 
+from hotels import router as router_hotels
+
 
 app = FastAPI()
 
-hotels = [
-    {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Дубай", "name": "dubai"},
-]
-
-@app.get("/hotels")
-def get_hotels(
-        id: int | None = Query(None, description="Айдишник"),
-        title: str | None = Query(None, description="Название отеля"),
-):
-    hotels_ = []
-    for hotel in hotels:
-        if id and hotel["id"] != id:
-            continue
-        if title and hotel["title"] != title:
-            continue
-        hotels_.append(hotel)
-    return hotels_
-
-
-@app.post("/hotels")
-def create_hotel(
-        title: str = Body(embed=True) #embed для передачи именно JSON
-):
-    global hotels
-    hotels.append({
-        "id": hotels[-1]["id"] + 1,
-        "title": title
-    })
-    return {"status": "OK"} # нужно возвращать JSON формат, а не строку
-
-
-@app.delete("/hotels/{hotel_id}")
-def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
-    return {"status": "OK"} # нужно возвращать JSON формат, а не строку
-
-
-@app.put("/hotels/{hotel_id}")
-def put_hotels(hotel_id: int,
-               title: str = Body(embed=True),
-               name: str = Body(embed=True)):
-    global hotels
-    for hotel in hotels:
-        if hotel["id"] == hotel_id:
-            hotel["title"] = title
-            hotel["name"] = name
-
-    return {"status": "OK"}
-
-
-@app.patch("/hotels/{hotel_id}")
-def patch_hotel(hotel_id: int,
-               title: str | None = Body(embed=True),
-               name: str | None = Body(embed=True)):
-    global hotels
-    for hotel in hotels:
-        if hotel["id"] == hotel_id and title:
-            hotel["title"] = title
-        elif hotel["id"] == hotel_id and name:
-            hotel["name"] = name
-        else:
-            return None
-
-    return {"status": "OK"}
+app.include_router(router_hotels)
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
